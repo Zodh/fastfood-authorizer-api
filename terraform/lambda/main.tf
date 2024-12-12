2f9594b9fe0b86c21ac7ab5da216d6f785f381f5
gui-meireles <guilhermemeirelesnascimento@gmail.com>
2024-12-11 21:04:50 -0300

    fix: api gateway

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -22,21 +22,16 @@ resource "aws_lambda_function" "eks_invoker" {
   }
 }
 
-# Cria recurso API Gateway
-resource "aws_api_gateway_rest_api" "eks_api" {
-  name = "EKS_API_Gateway"
-}
-
 # Recurso dinâmico base "/{proxy+}"
 resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = aws_api_gateway_rest_api.eks_api.id
-  parent_id   = aws_api_gateway_rest_api.eks_api.root_resource_id
+  rest_api_id = data.aws_api_gateway_rest_api.eks_api.id
+  parent_id   = data.aws_api_gateway_rest_api.eks_api.root_resource_id
   path_part   = "{proxy+}"
 }
 
 # Método para todas as requisições
 resource "aws_api_gateway_method" "proxy_method" {
-  rest_api_id   = aws_api_gateway_rest_api.eks_api.id
+  rest_api_id   = data.aws_api_gateway_rest_api.eks_api.id
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
@@ -44,7 +39,7 @@ resource "aws_api_gateway_method" "proxy_method" {
 
 # Integração com o Lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id = aws_api_gateway_rest_api.eks_api.id
+  rest_api_id = data.aws_api_gateway_rest_api.eks_api.id
   resource_id = aws_api_gateway_resource.proxy.id
   http_method = aws_api_gateway_method.proxy_method.http_method
 
@@ -62,7 +57,7 @@ resource "aws_lambda_permission" "api_gateway_permission" {
 }
 
 resource "aws_api_gateway_deployment" "eks_api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.eks_api.id
+  rest_api_id = data.aws_api_gateway_rest_api.eks_api.id
   stage_name  = "prod"
 
   depends_on = [
--- terraform/lambda/outputs.tf
+++ terraform/lambda/outputs.tf
@@ -1,6 +1,6 @@
 # Output para o endpoint HTTPS do API Gateway
 output "api_gateway_endpoint" {
-  value = "https://${aws_api_gateway_rest_api.eks_api.id}.execute-api.us-east-1.amazonaws.com/${aws_api_gateway_deployment.eks_api_deployment.stage_name}"
+  value = "https://${data.aws_api_gateway_rest_api.eks_api.id}.execute-api.us-east-1.amazonaws.com/${aws_api_gateway_deployment.eks_api_deployment.stage_name}"
   description = "Endpoint HTTPS global do API Gateway para acessar os recursos da API."
 }
 
